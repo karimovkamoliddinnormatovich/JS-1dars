9ef4d6c1cf0fa4e26535533c626a08b026b59099
Kamoliddin <kamoliddinkarimov25@gmail.com>
2022-04-02 10:45:37 +0500

    JS-1.dars

---
+++ script.js
@@ -0,0 +1,32 @@
+let ism = prompt('Isminingiz nima?')
+let ismingiz = 'Mening ismim '
+console.log(ismingiz + ism);
+let yosh = prompt('Necha yoshdasiz?')
+let yoshingiz1 = 'Men '
+let yoshingiz2 = ' yoshdaman'
+console.log(yoshingiz1 + yosh + yoshingiz2);
+
+let misol1 = prompt('1+5');
+let misol1_1 = 'Misol 1: 1+5=6 '
+let misol1_2 = 'Sizning javob '
+console.log(misol1_1+misol1_2+misol1);
+
+let misol2 = prompt('2+5');
+let misol2_1 = 'Misol 2: 2+5=7 '
+let misol2_2 = 'Sizning javob '
+console.log(misol2_1+misol2_2+misol2);
+
+let misol3 = prompt('1+4');
+let misol3_1 = 'Misol 3: 1+4=6 '
+let misol3_2 = 'Sizning javob '
+console.log(misol3_1+misol3_2+misol3);
+
+let misol4 = prompt('9-1');
+let misol4_1 = 'Misol 4: 9-1=8 '
+let misol4_2 = 'Sizning javob '
+console.log(misol4_1+misol4_2+misol4);
+
+let misol5 = prompt('1+3');
+let misol5_1 = 'Misol 5: 1+3=4 '
+let misol5_2 = 'Sizning javob '
+console.log(misol5_1+misol5_2+misol5);
